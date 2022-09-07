4942c7780397e0e724437ae331cbd641d0cd5c2b
liuyunan <liuyunan.20@gmail.com>
2022-09-07 12:08:01 -0700

    In this stage, your program should:
    Welcome users;
    Display the instructions;
    Ask for a request;
    Terminate the program if a user enters zero;
    If a number is not natural, print an error message;
    Print the properties of the natural number;
    Continue execution from step 3, after the request has been processed.
    The properties are even, odd, buzz, duck and palindromic. The tests won't check the order of properties, indentation, and spaces. You may format numbers as you like.

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -4,33 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int processInput() {
+    static long processInput() {
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.println("Enter a natural number:");
-            int num = scanner.nextInt();
-            if (num > 0) {
+            System.out.println("Enter a request:");
+            long num = scanner.nextLong();
+            if (num >= 0) {
                 return num;
             } else {
-                System.out.println("This number is not natural!");
-                return 0;
+                System.out.println("The first parameter should be a natural number or zero.");
+                return -1;
             }
         } catch (InputMismatchException e) {
-            System.out.println("This number is not natural!");
+            System.out.println("The first parameter should be a natural number or zero.");
             scanner.next();
-            return 0;
+            return -1;
         }
     }
 
-    static boolean checkParity(int num) {
+    static boolean checkParity(long num) {
         return num % 2 == 0;
     }
 
-    static boolean checkBuzz(int num) {
+    static boolean checkBuzz(long num) {
         return num % 7 == 0 || num % 10 == 7;
     }
 
-    static boolean checkDuck(int num) {
+    static boolean checkDuck(long num) {
         while (num / 10 > 0) {
             if (num % 10 == 0) {
                 return true;
@@ -41,14 +41,34 @@ public class Main {
         return false;
     }
 
-    public static void main(String[] args) {
-        int number = processInput();
-        if (number != 0) {
-            System.out.printf("Properties of %d\n", number);
-            System.out.printf("        even: %b\n", checkParity(number));
-            System.out.printf("         odd: %b\n", !checkParity(number));
-            System.out.printf("        buzz: %b\n", checkBuzz(number));
-            System.out.printf("        duck: %b\n", checkDuck(number));
+    static boolean checkPalindromic(long num) {
+        String s = String.valueOf(num);
+        int n = s.length();
+        while (n > 1) {
+            if (s.charAt(0) != s.charAt(n - 1)) {
+                return false;
+            }
+            s = s.substring(1, n -1);
+            n = s.length();
         }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Amazing Numbers!\n\nSupported requests:");
+        System.out.println("- enter a natural number to know its properties;\n- enter 0 to exit.\n");
+        long number;
+        do {
+            number = processInput();
+            if (number > 0) {
+                System.out.printf("Properties of %d\n", number);
+                System.out.printf("        even: %b\n", checkParity(number));
+                System.out.printf("         odd: %b\n", !checkParity(number));
+                System.out.printf("        buzz: %b\n", checkBuzz(number));
+                System.out.printf("        duck: %b\n", checkDuck(number));
+                System.out.printf(" palindromic: %b\n", checkPalindromic(number));
+            }
+        } while (number != 0);
+        System.out.println("Goodbye!");
     }
 }
