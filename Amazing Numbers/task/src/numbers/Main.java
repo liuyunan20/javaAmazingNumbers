f71cac5cce0f63af3848c7f10550530c4d49e416
liuyunan <liuyunan.20@gmail.com>
2022-09-16 15:28:11 -0700

    Your program should process various user requests. In this stage, your program should:
    Welcome users;
    Display the instructions;
    Ask for a request;
    If a user enters zero, terminate the program;
    If a user enters an empty request, print the instructions;
    If numbers are not natural, print an error message;
    If one number is entered, calculate and print the properties of this number;
    For two numbers, print the list of numbers with properties;
    Once the request is processed, continue execution from step 3.
    In the current stage, the property names include even, odd, buzz , duck, palindromic and gapful. The test won't check the order of properties, their indentation, and spaces.

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,24 +1,33 @@
 package numbers;
-import java.util.InputMismatchException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
 
-    static long processInput() {
+    static List<Long> processInput() {
+        System.out.println("Enter a request:");
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.println("Enter a request:");
-            long num = scanner.nextLong();
-            if (num >= 0) {
-                return num;
-            } else {
+            List<Long> nums = Arrays.stream(scanner.nextLine().split(" "))
+                    .map(Long::valueOf).toList();
+            if ((nums.size() == 1 && nums.get(0) >= 0) ||
+                    (nums.size() == 2 && nums.get(0) > 0 && nums.get(1) > 0)) {
+                return nums;
+            } else if (nums.get(0) < 0){
                 System.out.println("The first parameter should be a natural number or zero.");
-                return -1;
+                return null;
+            } else if (nums.size() == 2 && nums.get(1) <= 0){
+                System.out.println("The second parameter should be a natural number or zero.");
+                return null;
+            } else {
+                System.out.println("You need to input one or two natural numbers or zero.");
+                return null;
             }
-        } catch (InputMismatchException e) {
+        } catch (NumberFormatException e) {
             System.out.println("The first parameter should be a natural number or zero.");
-            scanner.next();
-            return -1;
+            return null;
         }
     }
 
@@ -54,21 +63,54 @@ public class Main {
         return true;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Amazing Numbers!\n\nSupported requests:");
-        System.out.println("- enter a natural number to know its properties;\n- enter 0 to exit.\n");
-        long number;
-        do {
-            number = processInput();
-            if (number > 0) {
-                System.out.printf("Properties of %d\n", number);
-                System.out.printf("        even: %b\n", checkParity(number));
-                System.out.printf("         odd: %b\n", !checkParity(number));
-                System.out.printf("        buzz: %b\n", checkBuzz(number));
-                System.out.printf("        duck: %b\n", checkDuck(number));
-                System.out.printf(" palindromic: %b\n", checkPalindromic(number));
+    static boolean checkGapful(long num) {
+        if (num >= 100L) {
+            long d0 = num % 10;
+            long d1 = num;
+            while (d1 > 9L) {
+                d1 = d1 / 10L;
             }
-        } while (number != 0);
+            return num % (d1 * 10L + d0) == 0L;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("""
+                Welcome to Amazing Numbers!
+                
+                Supported requests:
+                - enter a natural number to know its properties;
+                - enter two natural numbers to obtain the properties of the list:
+                * the first parameter represents a starting number;
+                * the second parameter shows how many consecutive numbers are to be processed;
+                - separate the parameters with one space;
+                - enter 0 to exit.""");
+        List<Long> number;
+
+        do {
+            do {
+                number = processInput();
+            } while (number == null);
+            if (number.get(0) != 0 && number.size() == 1) {
+                System.out.printf("Properties of %d\n", number.get(0));
+                System.out.printf("        buzz: %b\n", checkBuzz(number.get(0)));
+                System.out.printf("        duck: %b\n", checkDuck(number.get(0)));
+                System.out.printf(" palindromic: %b\n", checkPalindromic(number.get(0)));
+                System.out.printf("      gapful: %b\n", checkGapful(number.get(0)));
+                System.out.printf("        even: %b\n", checkParity(number.get(0)));
+                System.out.printf("         odd: %b\n", !checkParity(number.get(0)));
+            } else if (number.size() == 2) {
+                for (int i = 0; i < number.get(1); i++) {
+                    System.out.printf("%d is ", number.get(0) + i);
+                    System.out.print(checkBuzz(number.get(0) + i)?"buzz, ": "");
+                    System.out.print(checkDuck(number.get(0) + i)?"duck, ": "");
+                    System.out.print(checkPalindromic(number.get(0) + i)?"palindromic, ": "");
+                    System.out.print(checkGapful(number.get(0) + i)?"gapful, ": "");
+                    System.out.print(checkParity(number.get(0) + i)?"even\n": "odd\n");
+                }
+            }
+        } while (number.get(0) != 0);
         System.out.println("Goodbye!");
     }
 }
